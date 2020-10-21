24b55f0a85d865850f0802711e0969a3c7972f62
Gustavo Assis <assiscs2@gmail.com>
2020-10-21 12:46:18 -0300

    master

---
+++ src/database/fakedata.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+    id: 1,
+    lat: "-27.2152821",
+    lng: "-49.6492667",
+    name: "Lar das meninas",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+        "https://images.unsplash.com/photo-1567701562484-deab2750d1e8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+        
+        "https://images.unsplash.com/photo-1592840331052-16e15c2c6f95?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ], 
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 18h até 8h",
+    open_on_weekends: "1"
+
+},
+
+{
+    id: 2,
+    lat: "-27.2152821",
+    lng: "-49.6592667",
+    name: "Lar dos meninos",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+        "https://images.unsplash.com/photo-1567701562484-deab2750d1e8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+        
+        "https://images.unsplash.com/photo-1592840331052-16e15c2c6f95?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ], 
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 18h até 8h",
+    open_on_weekends: "0"
+}
+]
